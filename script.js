360995919c4d65c1bd4ec0047f1bdf5e6c2a8657
kumaraakarshan <aakarshan0005@gmail.com>
2023-07-29 18:22:50 +0530

    delete button and its functionality added

--- script.js
+++ script.js
@@ -14,23 +14,35 @@
 function displayLocalStorageData() {
     const itemList = document.getElementById('items');
     itemList.innerHTML = ''; // Clear the existing list items
+    itemList.addEventListener('click', removeItem);
 
     // Loop through localStorage and display the data
     for (let i = 0; i < localStorage.length; i++) {
-      const key = localStorage.key(i);
-      const data = JSON.parse(localStorage.getItem(key));
-
-      const li = document.createElement('li');
-      li.className = 'list-group-item';
-      const combinedText = `${data.name} ${data.Email}-${data.Phone} (${data.date} at ${data.Time})`;
-      li.appendChild(document.createTextNode(combinedText));
-
-      itemList.appendChild(li);
+        const key = localStorage.key(i);
+        const data = JSON.parse(localStorage.getItem(key));
+        var deleteBtn = document.createElement('button');
+        const li = document.createElement('li');
+        li.className = 'list-group-item';
+        const combinedText = `${data.name} ${data.Email}-${data.Phone} (${data.date} at ${data.Time})`;
+        li.appendChild(document.createTextNode(combinedText));
+        deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+        deleteBtn.dataset.key = key; // Store the key as a dataset attribute
+        deleteBtn.appendChild(document.createTextNode('delete'));
+        li.appendChild(deleteBtn);
+        itemList.appendChild(li);
     }
-  }
+}
 
-
-
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are you sure?')) {
+            const key = e.target.dataset.key; // Get the key from the dataset attribute
+            localStorage.removeItem(key);
+            const li = e.target.parentElement;
+            li.remove();
+        }
+    }
+}
 document.getElementById("submitBtn").addEventListener("click", function() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
@@ -38,30 +50,19 @@ document.getElementById("submitBtn").addEventListener("click", function() {
     const date = document.getElementById("date").value;
     const timeForCall = document.getElementById("time_for_call").value;
 
-    // const message = `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nTime for Call: ${timeForCall}`;
-    // localStorage.setItem('Name',`${name}`);
-    // localStorage.setItem('Email',`${email}`);
-    // localStorage.setItem('Phone',`${phone}`);
-    // localStorage.setItem('Time For Call',`${timeForCall}`);
     const uniqueKey = Date.now().toString();
-    let myObj ={
-        name  : name,
-        Email : email,
-        Phone : phone,
-        date  : date,
-        Time  : timeForCall
-
+    let myObj = {
+        name: name,
+        Email: email,
+        Phone: phone,
+        date: date,
+        Time: timeForCall
     };
-
-    let myObj_serialized =JSON.stringify(myObj);
-    
-    localStorage.setItem(uniqueKey,myObj_serialized);
-    
+    let myObj_serialized = JSON.stringify(myObj);
+    localStorage.setItem(uniqueKey, myObj_serialized);
     displayLocalStorageData();
-
-    let myObj_deserialized=JSON.parse(localStorage.getItem("myObj"));
+    let myObj_deserialized = JSON.parse(localStorage.getItem(uniqueKey));
     console.log(myObj_deserialized);
-    
 });
-displayLocalStorageData();
 
+displayLocalStorageData();
