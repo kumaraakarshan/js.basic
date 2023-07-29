647f330ee2dcb26e9e036957d9356928549061e7
kumaraakarshan <aakarshan0005@gmail.com>
2023-07-29 10:00:09 +0530

    storing data using object

--- script.js
+++ script.js
@@ -16,9 +16,25 @@ document.getElementById("submitBtn").addEventListener("click", function() {
     const phone = document.getElementById("phone").value;
     const timeForCall = document.getElementById("time_for_call").value;
 
-    const message = `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nTime for Call: ${timeForCall}`;
-    localStorage.setItem('Name',`${name}`);
-    localStorage.setItem('Email',`${email}`);
-    localStorage.setItem('Phone',`${phone}`);
-    localStorage.setItem('Time For Call',`${timeForCall}`);
+    // const message = `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nTime for Call: ${timeForCall}`;
+    // localStorage.setItem('Name',`${name}`);
+    // localStorage.setItem('Email',`${email}`);
+    // localStorage.setItem('Phone',`${phone}`);
+    // localStorage.setItem('Time For Call',`${timeForCall}`);
+
+    let myObj ={
+        name  : `${name}`,
+        Email : `${email}`,
+        Phone : `${phone}`,
+        Time: `${timeForCall}`
+
+    };
+
+    let myObj_serialized =JSON.stringify(myObj);
+    
+    localStorage.setItem("myObj",myObj_serialized);
+
+    let myObj_deserialized=JSON.parse(localStorage.getItem("myObj"));
+    console.log(myObj_deserialized);
+    
 });
