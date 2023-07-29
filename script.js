636e48c798a0e72ea52ecb2151e20422b3be221d
kumaraakarshan <aakarshan0005@gmail.com>
2023-07-29 11:26:10 +0530

    multiple user displayed on screen and local storage

--- script.js
+++ script.js
@@ -10,10 +10,32 @@
 // document.cookie = 'lastname=aaku; expires=' + new Date(2030, 0, 1).toUTCString();
 // console.log(document.cookie);
 
+
+function displayLocalStorageData() {
+    const itemList = document.getElementById('items');
+    itemList.innerHTML = ''; // Clear the existing list items
+
+    // Loop through localStorage and display the data
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const data = JSON.parse(localStorage.getItem(key));
+
+      const li = document.createElement('li');
+      li.className = 'list-group-item';
+      const combinedText = `${data.name} ${data.Email}-${data.Phone} (${data.date} at ${data.Time})`;
+      li.appendChild(document.createTextNode(combinedText));
+
+      itemList.appendChild(li);
+    }
+  }
+
+
+
 document.getElementById("submitBtn").addEventListener("click", function() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
+    const date = document.getElementById("date").value;
     const timeForCall = document.getElementById("time_for_call").value;
 
     // const message = `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nTime for Call: ${timeForCall}`;
@@ -21,20 +43,25 @@ document.getElementById("submitBtn").addEventListener("click", function() {
     // localStorage.setItem('Email',`${email}`);
     // localStorage.setItem('Phone',`${phone}`);
     // localStorage.setItem('Time For Call',`${timeForCall}`);
-
+    const uniqueKey = Date.now().toString();
     let myObj ={
-        name  : `${name}`,
-        Email : `${email}`,
-        Phone : `${phone}`,
-        Time: `${timeForCall}`
+        name  : name,
+        Email : email,
+        Phone : phone,
+        date  : date,
+        Time  : timeForCall
 
     };
 
     let myObj_serialized =JSON.stringify(myObj);
     
-    localStorage.setItem("myObj",myObj_serialized);
+    localStorage.setItem(uniqueKey,myObj_serialized);
+    
+    displayLocalStorageData();
 
     let myObj_deserialized=JSON.parse(localStorage.getItem("myObj"));
     console.log(myObj_deserialized);
     
 });
+displayLocalStorageData();
+
